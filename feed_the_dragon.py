1c7ce8496c4de3cfa36ebb15450038d0c9f41454
Tim Moriarty <timmoriarty@gmail.com>
2024-05-17 16:01:29 -0500

    Asset setup and HUD creation

--- feed_the_dragon.py
+++ feed_the_dragon.py
@@ -20,7 +20,7 @@ PLAYER_STARTING_LIVES = 5
 PLAYER_VELOCITY = 5
 COIN_STARTING_VELOCITY = 5
 COIN_ACCELERATION = .5
-BUFFER_DISTANCE = 100
+BUFFER_DISTANCE = -50
 
 score = 0
 player_lives = PLAYER_STARTING_LIVES
@@ -45,23 +45,37 @@ title_rect = title_text.get_rect()
 title_rect.centerx = WINDOW_WIDTH//2
 title_rect.y = 10
 
-lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARKGREEN)
+lives_text = font.render('Lives: ' + str(player_lives), True, GREEN, DARKGREEN)
 lives_rect = lives_text.get_rect()
 lives_rect.topright = (WINDOW_WIDTH - 10, 10)
 
 
-#Set Sounds and Music
+game_over_text = font.render('GAMEOVER', True, GREEN, DARKGREEN)
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
 
+continue_text = font.render('Press any key to play again', True, GREEN, DARKGREEN)
+continue_rect = continue_text.get_rect()
+continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 32)
+
+
+
+#Set Sounds and Music
+coin_sound = pygame.mixer.Sound('coin_sound.wav')
+miss_sound = pygame.mixer.Sound('miss_sound.wav')
+miss_sound.set_volume(.1)
+pygame.mixer.music.load('ftd_background_music.wav')
 
 
 #Set Images
-dragon_image = pygame.image.load("dragon_right.png")
-dragon_rect = dragon_image.get_rect()
-dragon_rect.topleft = (25,25)
+player_image = pygame.image.load("dragon_right.png")
+player_rect = player_image.get_rect()
+player_rect.left = 32
+player_rect.centery = WINDOW_HEIGHT//2
 
 coin_image = pygame.image.load("coin.png")
 coin_rect = coin_image.get_rect()
-coin_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+coin_rect.center = (WINDOW_WIDTH + BUFFER_DISTANCE, random.randint(64, WINDOW_HEIGHT-32))
 
 
 
@@ -74,36 +88,44 @@ while running:
             running = False
 
     #Get a list of all keys currently being pressed down
-    keys = pygame.key.get_pressed()   
+    #keys = pygame.key.get_pressed()   
     
 
     #Move the dragon continously
-    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and dragon_rect.left > 0:
-        dragon_rect.x -= PLAYER_VELOCITY
-    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and dragon_rect.right < WINDOW_WIDTH:
-        dragon_rect.x += PLAYER_VELOCITY
-    if (keys[pygame.K_UP] or keys[pygame.K_w]) and dragon_rect.top > 0:
-        dragon_rect.y -= PLAYER_VELOCITY
-    if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and dragon_rect.bottom < WINDOW_HEIGHT:
-        dragon_rect.y += PLAYER_VELOCITY
+    #if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and dragon_rect.left > 0:
+    #    dragon_rect.x -= PLAYER_VELOCITY
+    #if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and dragon_rect.right < WINDOW_WIDTH:
+    #    dragon_rect.x += PLAYER_VELOCITY
+    #if (keys[pygame.K_UP] or keys[pygame.K_w]) and dragon_rect.top > 0:
+    #    dragon_rect.y -= PLAYER_VELOCITY
+    #if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and dragon_rect.bottom < WINDOW_HEIGHT:
+    #    dragon_rect.y += PLAYER_VELOCITY
 
 
     #Check for collission between two rects
-    if dragon_rect.colliderect(coin_rect):
-        print("HIT")
-        coin_rect.left = random.randint(0, WINDOW_WIDTH - 48)
-        coin_rect.top = random.randint(0, WINDOW_HEIGHT - 48)
+    #if dragon_rect.colliderect(coin_rect):
+    #    print("HIT")
+    #    coin_rect.left = random.randint(0, WINDOW_WIDTH - 48)
+    #    coin_rect.top = random.randint(0, WINDOW_HEIGHT - 48)
 
     #Fill the display surface to cover old images
     display_surface.fill((0,0,0))
 
     #Draw rectangles to represent rectangles
-    pygame.draw.rect(display_surface, (0,255,0), dragon_rect, 1)
-    pygame.draw.rect(display_surface, (255,0,0), coin_rect, 1)
+    #pygame.draw.rect(display_surface, (0,255,0), dragon_rect, 1)
+    #pygame.draw.rect(display_surface, (255,0,0), coin_rect, 1)
+    
+    #Blit the HUD to the screen
+    display_surface.blit(score_text, score_rect)
+    display_surface.blit(title_text, title_rect)
+    display_surface.blit(lives_text, lives_rect)
+    pygame.draw.line(display_surface, WHITE, (0,64), (WINDOW_WIDTH, 64), 2)
+
     
     #Blit (copy) assets to the screen
-    display_surface.blit(dragon_image, dragon_rect)
+    display_surface.blit(player_image, player_rect)
     display_surface.blit(coin_image, coin_rect)
+
 
     #update display
     pygame.display.update()
